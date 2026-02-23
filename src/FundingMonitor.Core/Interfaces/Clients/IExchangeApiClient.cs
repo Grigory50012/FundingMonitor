e2e177d32f9645b003b82aecdfd2ad54edb86737
gk2979196@gmail.com <gk2979196@gmail.com>
2026-02-23 11:40:43 +0300

    Добавил запросы для получения истории ставок

--- src/FundingMonitor.Core/Interfaces/Clients/IExchangeApiClient.cs
+++ src/FundingMonitor.Core/Interfaces/Clients/IExchangeApiClient.cs
@@ -1,11 +1,18 @@
 using FundingMonitor.Core.Entities;
 
-namespace FundingMonitor.Application.Interfaces.Clients;
+namespace FundingMonitor.Core.Interfaces.Clients;
 
 public interface IExchangeApiClient
 {
     ExchangeType ExchangeType { get; }
-
     Task<List<CurrentFundingRate>> GetCurrentFundingRatesAsync(CancellationToken cancellationToken);
+
+    Task<List<HistoricalFundingRate>> GetHistoricalFundingRatesAsync(
+        string symbol,
+        DateTime fromTime,
+        DateTime toTime,
+        int limit,
+        CancellationToken cancellationToken);
+
     Task<bool> IsAvailableAsync(CancellationToken cancellationToken);
 }
